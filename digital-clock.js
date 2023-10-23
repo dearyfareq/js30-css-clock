7b7ee004f7ef8fc6b293f7aa74d963cf32dc4228
Diyari <dearyfareq15@gmail.com>
2023-10-23 13:14:22 +0300

    added js functions, comments will be added

---
+++ digital-clock.js
@@ -0,0 +1,65 @@
+function updateTime() {
+  // Array of months
+  const months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+
+  // Array of days of the week
+  const daysOfWeek = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  const time = new Date();
+  const day = time.getDate();
+  const month = time.getMonth();
+  const year = time.getFullYear();
+  const weekday = time.getDay();
+  const hour = time.getHours();
+  const minute = time.getMinutes();
+  const second = time.getSeconds();
+
+  var data = [day, month, year, weekday, hour, minute, second];
+  var elements = [
+    "day",
+    "month",
+    "year",
+    "weekday",
+    "hour",
+    "minute",
+    "second",
+  ];
+
+  for (var i = 0; i < elements.length; i++) {
+    var change = document.getElementById(elements[i]);
+    if (elements[i] == "month") {
+      change.innerText = months[data[1]] + ",";
+    } else if (elements[i] == "weekday") {
+      change.innerText = daysOfWeek[data[3]];
+    } else if (elements[i] == "hour") {
+      change.innerText = data[i] + ":";
+    } else if (elements[i] == "minute") {
+      change.innerText = data[i] + ":";
+    } else {
+      change.innerText = data[i];
+    }
+  }
+}
+
+setInterval(updateTime, 1000);
